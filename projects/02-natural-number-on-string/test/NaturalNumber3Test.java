3f7909c2ff4a06fce45832cb386b18ea5f3f0e96
Allen Thomas <allenthom05@gmail.com>
2024-02-01 15:55:58 -0500

    Update NaturalNumber3Test.java

--- projects/02-natural-number-on-string/test/NaturalNumber3Test.java
+++ projects/02-natural-number-on-string/test/NaturalNumber3Test.java
@@ -1,4 +1,9 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import components.naturalnumber.NaturalNumber;
+import components.naturalnumber.NaturalNumber1L;
 
 /**
  * Customized JUnit test fixture for {@code NaturalNumber3}.
@@ -76,5 +81,41 @@ public class NaturalNumber3Test extends NaturalNumberTest {
         // This line added just to make the component compilable.
         return null;
     }
+    @Test
+    public void multiplyBy10TestInt() {
+        NaturalNumber test = this.constructorTest(9);
+        NaturalNumber actual = this.constructorRef(99);
+        test.multiplyBy10(9);
 
+
+        assertEquals(test, actual);
+    }
+
+    @Test
+    public void divideBy10TestStr() {
+        NaturalNumber test = this.constructorTest("81");
+        NaturalNumber actual = this.constructorRef("8");
+        test.divideBy10();
+
+
+        assertEquals(test, actual);
+    }
+    @Test
+    public void divideBy10TestNN() {
+        NaturalNumber numTest = new NaturalNumber1L(23);
+        NaturalNumber test = this.constructorTest(numTest);
+        NaturalNumber numActual = new NaturalNumber1L(2);
+        NaturalNumber actual = this.constructorRef(numActual);
+        test.divideBy10();
+
+
+        assertEquals(test, actual);
+    }
+
+    @Test
+    public void isZeroTestEmpty() {
+        NaturalNumber test = this.constructorTest();
+
+        assertEquals(test.isZero(), true);
+    }
 }
