25b8230f261845d57f7fb161b52cb503faa20670
EashanVytla <Eashan.Vytla@gmail.com>
2024-02-16 14:24:18 -0500

    Sandbox

---
+++ projects/Sandbox/src/ProgramSkeleton.java
@@ -0,0 +1,34 @@
+import components.simplewriter.*;
+
+/**
+ * Put a short phrase describing the program here.
+ *
+ * @author Put your name here
+ *
+ */
+public final class ProgramSkeleton {
+
+    /**
+     * No argument constructor--private to prevent instantiation.
+     */
+    private ProgramSkeleton() {
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+         * Put your main program code here
+         */
+        SimpleWriter out = new SimpleWriter1L();
+
+        out.println("Hello World");
+
+        out.close();
+    }
+
+}
