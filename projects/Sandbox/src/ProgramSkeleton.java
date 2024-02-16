d69303155bbab95dd7ad8c1199e39ddf5e8ea77a
Allen Thomas <allenthom05@gmail.com>
2024-02-16 15:08:30 -0500

    test

--- projects/Sandbox/src/ProgramSkeleton.java
+++ projects/Sandbox/src/ProgramSkeleton.java
@@ -1,4 +1,5 @@
-import components.simplewriter.*;
+import components.binarytree.BinaryTree;
+import components.binarytree.BinaryTree1;
 
 /**
  * Put a short phrase describing the program here.
@@ -24,11 +25,34 @@ public final class ProgramSkeleton {
         /*
          * Put your main program code here
          */
-        SimpleWriter out = new SimpleWriter1L();
+        BinaryTree<Integer> r = new BinaryTree1<>();
+        r.assemble(null, r, r);
 
-        out.println("Hello World");
-
-        out.close();
     }
 
+
+    /**
+     * Returns whether the given tree is balanced.
+     *
+     * @ensures isBalanced = [true if t is balanced, false otherwise]
+     */
+    private static <T> boolean isBalanced(BinaryTree<T> t) {
+        assert t != null : "Violation of: t is not null";
+        boolean balanced = true;
+        if(t.size() != 0){
+            BinaryTree<T> left = t.newInstance();
+            BinaryTree<T> right = t.newInstance();
+            T root = t.disassemble(left, right);
+
+            if(Math.abs(right.height() - left.height()) <= 1){
+                balanced = isBalanced(left) && isBalanced(right);
+            }else{
+                balanced = false;
+            }
+            t.assemble(root,left,right);
+        }
+
+        return balanced;
+    }
 }
+
